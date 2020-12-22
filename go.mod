bdf380f74692a2f71baa24d589469c373d07fe94
flc1125 <four_leaf_clover@foxmail.com>
2020-12-23 00:15:11 +0800

    redis

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/flc1125/redis-batch-delete
 
 go 1.15
 
-require github.com/urfave/cli/v2 v2.3.0
+require (
+	github.com/go-redis/redis/v8 v8.4.4
+	github.com/urfave/cli/v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/flc1125/redis-batch-delete/commands"
+	"github.com/go-redis/redis/v8"
 	"github.com/urfave/cli/v2"
 )
 
+var ctx = context.Background()
+
+func redisClient() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	keys, _, _ := rdb.Scan(ctx, 0, "*", 10).Result()
+
+	fmt.Println(keys)
+}
+
 func main() {
+
+	redisClient()
+
 	app := &cli.App{
 		Name:   "boom",
 		Usage:  "make an explosive entrance",
